1b711259f4952dff02aafb44d685106aa58eadc0
Alexei <0alexchebanu@gmail.com>
2022-10-23 18:31:06 +0300

    initial

---
+++ UnitTest_/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using lesson13_14;
+
+namespace lesson13UnitTests
+{
+    public class Lesson13TESTS
+    {
+        [Fact]
+        public void test1()
+        {
+            CustomerData customer = new CustomerData();
+            string? result = customer.ToString();
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void test2()
+        {
+            ProductInfo product = new ProductInfo();
+            string? result = product.ToString();
+            Assert.Contains(result, "Product");
+        }
+       
+
+    }
+}
+
